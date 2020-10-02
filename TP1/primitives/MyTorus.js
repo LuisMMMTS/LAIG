67d4ba23437593363c9a24d97e80bdd1d7047e91
Rita Peixoto <up201806257@fe.up.pt>
2020-10-02 21:58:27 +0100

    Done Torus, missing verification on indices

--- TP1/primitives/MyTorus.js
+++ TP1/primitives/MyTorus.js
@@ -1,10 +1,3 @@
-/*
-- centrado na origem
-- simetria à volta do eixo dos Z
-- raio interior:
-- raio exterior:
-*/
-
 /**
  * MyTorus
  * @constructor
@@ -27,7 +20,45 @@ class MyTorus extends CGFobject {
 	}
 	
 	initBuffers() {
+		this.vertices = [];
+		this.indices = [];
+		this.normals = [];
+		this.texCoords = [];
+	
+		for(var n = 0; n <= this.loops; n++){
+			var theta = n*2*Math.PI/this.loops;
+
+
+			for(var s=0; s <= this.slices; s++){
+				var phi = s*2*Math.PI/this.slices;
+				var x = (this.outer + (this.inner * Math.cos(theta))) * Math.cos(phi);
+				var y = (this.outer + (this.inner * Math.cos(theta))) * Math.sin(phi);
+				var z = this.inner * Math.sin(theta);
+
+				var k  = n/this.loops;
+				var t  = s/this.slices;	
+
+				this.vertices.push(x,y,z);
+				this.normals.push(x,y,z);
+				this.texCoords.push(t,k);
+			}
+			
+		}
+
+		/*verify indices*/ 
+		for(var n=0; n <= this.loops; n++){
+			for(var s=0; s<=this.slices; s++){	
+				var i = n*(this.slices+1) + s;
+				var j = i + this.slices + 1;
+
+				this.indices.push(i, j+1, j);
+				this.indices.push(i, i+1, j+1);
+			}
+			
+		}
 		
+		this.primitiveType = this.scene.gl.TRIANGLES;
+		this.initGLBuffers();
 	}
 
 }
