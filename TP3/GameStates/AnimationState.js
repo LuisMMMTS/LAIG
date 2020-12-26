0659b750183f2ae9bed127808c6f67a07345602c
LuisMMMTS <torres.luismiguel@gmail.com>
2020-12-26 19:32:52 +0000

    resolved animation problem when changing board animation would not delete from piece

--- TP3/GameStates/AnimationState.js
+++ TP3/GameStates/AnimationState.js
@@ -16,30 +16,24 @@ class AnimationState extends GameState{
         //verificar se terminou o jogo --> se sim, gameOver State
         this.orchestrator.previousObj.update(time)
         this.orchestrator.finalObj.update(time)
-        //console.log("Anumation ended?");
         if(this.orchestrator.previousObj.animation.active){
-            console.log("Anumation 1 is active");
             if(this.orchestrator.previousObj.animation.ended){
-                console.log("Anumation 1 is ended");
-                this.orchestrator.previousObj.active = false
-                //this.orchestrator.gameBoard.addPieceToTile(this.orchestrator.previousObj, this.orchestrator.finalTile)
-                console.log("added initial piece to final tile")
                 this.orchestrator.previousObj.pick()
             }
         } 
 
         if(this.orchestrator.finalObj.animation.active){
-            //console.log("Anumation 2 is active");
             if(this.orchestrator.finalObj.animation.ended){
-                console.log("Anumation 2 is ended");
-                this.orchestrator.finalObj.active = false
-                //this.orchestrator.gameBoard.addPieceToTile(this.orchestrator.finalObj, this.orchestrator.startTile)
-                console.log("added final piece to initial tile")
                 this.orchestrator.finalObj.pick()
             }
         } 
         if (this.orchestrator.previousObj.animation.ended && this.orchestrator.finalObj.animation.ended){
+            this.orchestrator.previousObj.animation=null
+            this.orchestrator.finalObj.animation=null
+            this.orchestrator.gameBoard.switchTiles(this.orchestrator.startTile,this.orchestrator.finalTile);
+
             this.orchestrator.changeState(new ReadyState(this.orchestrator));//maybe here should be checking if game is over
+            
             //this.orchestrator.prolog.getWinner(this.orchestrator.gameBoard, this.orchestrator.currentPlayer);
         }
     }
